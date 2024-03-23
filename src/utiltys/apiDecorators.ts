2f218f43354ebbf5f8bcfe0987ab964f91d60cc5
Lee Seung Jin <dltmdwls154@gmail.com>
2024-03-23 21:47:12 +0900

    1Day 1Commit

--- src/utiltys/apiDecorators.ts
+++ src/utiltys/apiDecorators.ts
@@ -11,18 +11,13 @@ export const controller = (tag: string, path: string): MethodDecorator => {
 };
 
 /**
- * @param {string} name
- * @param {boolean} required
- * @param {any} type
- * @param {string} description
+ * @param {string} name 쿼리 이름
+ * @param {boolean} required 필수 여부
+ * @param {any} type 타입
+ * @param {string} description 설명
  * @returns {MethodDecorator }
  */
-export const apiQuery = (
-  name: string,
-  required: boolean,
-  type: any,
-  description: string
-): MethodDecorator & ClassDecorator => {
+export const apiQuery = (name: string, required: boolean, type: any, description: string): MethodDecorator => {
   return ApiQuery({ name, required, type, description });
 };
 
@@ -37,26 +32,27 @@ export const apiParam = (name: string, required: boolean, description: string): 
 };
 
 /**
- * @param {String} summary 설명
- * @param {String} description 설명
+ * @param {String} summary API 요약
+ * @param {String} description API 설명
+ * @returns {MethodDecorator}
  * */
-export const apiOperation = (summary: string, description: string) => {
+export const apiOperation = (summary: string, description: string): MethodDecorator => {
   return ApiOperation({ summary, description });
 };
 
 /**
- * @param {number} status
- * @param {string} description
- * @param {any} type
- * @returns {MethodDecorator & ClassDecorator}
+ * @param {number} status HTTP 상태 코드
+ * @param {string} description API 설명
+ * @param {any} type 반환 타입
+ * @returns {MethodDecorator }
  */
-export const apiResponse = (status: number, description: string, type?: any): MethodDecorator & ClassDecorator => {
+export const apiResponse = (status: number, description: string, type?: any): MethodDecorator => {
   return ApiResponse({ status, description, type });
 };
 
 /**
- * @param {string} description
- * @param {any} type
+ * @param {string} description API Request 설명
+ * @param {any} type API Request 타입
  * @returns { MethodDecorator }
  */
 export const apiRequest = (description: string, type: any): MethodDecorator => {
@@ -64,8 +60,10 @@ export const apiRequest = (description: string, type: any): MethodDecorator => {
 };
 
 /**
- * @returns {MethodDecorator }
  * @param options {description: string; type: any}
+ * @option description {string} API Body 설명
+ * @option type {any} API Body 타입
+ * @returns {MethodDecorator }
  */
 export const apiBody = (options: { description: string; type: any }): MethodDecorator => {
   return ApiBody(options);
