576251b12ad60be688f8e794837411162e2f4234
Lee Seung Jin <dltmdwls154@gmail.com>
2024-03-29 23:54:21 +0900

    1Dau 2Commit

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -11,6 +11,14 @@ export class MembersController {
   constructor(private readonly membersService: MembersService) {}
 
   @Get()
+  @apiOperation("멤버 전체 조회", "멤버 전체를 조회한다.")
+  @apiResponse(200, "멤버 전체 조회", Member)
+  @apiResponse(404, "멤버 전체 조회 실패")
+  @apiResponse(500, "서버 에러")
+  @apiResponse(400, "잘못된 요청")
+  @apiResponse(401, "권한 없음")
+  @apiResponse(403, "금지됨")
+  @apiResponse(405, "허용되지 않음")
   getAll(@Query("page") page: number, @Query("size") size: number) {
     return this.membersService.getAll({ page: 1, size: 10 });
   }
--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -1,13 +1,25 @@
 import { Module } from "@nestjs/common";
 import { MembersService } from "./members.service";
-import { MembersController } from "./members.controller";
+import {
+  MembersController,
+  MembersDeleteController,
+  MembersDetailController,
+  MembersPatchController,
+  MembersPostController,
+} from "./members.controller";
 import MembersSchema from "./members.schema";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Member } from "./entities/Members.entity";
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Member.name, schema: MembersSchema.schema }])],
-  controllers: [MembersController],
+  controllers: [
+    MembersController,
+    MembersDetailController,
+    MembersDeleteController,
+    MembersPatchController,
+    MembersPostController,
+  ],
   providers: [MembersService, MembersSchema],
 })
 export class MembersModule {}
