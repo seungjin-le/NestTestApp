93bbae38c0c3a73d3f54f3f9c6d78a1dabaf3d44
Lee Seung Jin <dltmdwls154@gmail.com>
2024-04-11 23:55:33 +0900

    1Day 1Commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
-import { MongooseModule } from "@nestjs/mongoose";
-import { Auth } from "./entities/auth.entity";
-import AuthSchema from "./auth.schema";
 import { UserModule } from "../user/user.module";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema.schema }]), UserModule],
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
