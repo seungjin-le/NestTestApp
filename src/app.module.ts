7a5573bfadcf2020e4069d6d32b31ac4d7a537f3
Lee Seung Jin <dltmdwls154@gmail.com>
2024-05-04 23:37:04 +0900

    file 이름 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { ConfigModule } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
-import { FileModule } from './file/file.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
 
 @Module({
   imports: [
@@ -24,7 +24,7 @@ import { FileModule } from './file/file.module';
         uri: process.env.MONGODB_URL,
       }),
     }),
-    FileModule,
+    FileUploadModule,
   ],
   controllers: [],
   providers: [],
