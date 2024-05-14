5ecb7caf6e14ae873f4209d30781f9be99ca6838
Lee Seung Jin <dltmdwls154@gmail.com>
2024-05-14 23:54:18 +0900

    file 스키마 추가

---
+++ src/file-upload/file-upload.schema.ts
@@ -0,0 +1,24 @@
+import mongoose, { SchemaOptions, Schema, Document } from "mongoose";
+
+const options: SchemaOptions = {
+  timestamps: true,
+  collection: "file-upload",
+};
+
+export interface FileUploadDocument extends Document {
+  name: string;
+  path: string;
+  size: number;
+}
+
+const Member = new Schema<FileUploadDocument>(
+  {
+    name: { type: String, required: true },
+    path: { type: String, required: true },
+    size: { type: Number, required: true },
+  },
+  options
+);
+
+const FileUploadSchema = mongoose.model<FileUploadDocument>("FileUpload", Member);
+export default FileUploadSchema;
