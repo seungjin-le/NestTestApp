880f987369394ca8471686cec8c53310bfc7978f
Lee Seung Jin <dltmdwls154@gmail.com>
2024-05-21 23:37:12 +0900

    Test

--- src/file-upload/dto/create.dto.ts
+++ src/file-upload/dto/create.dto.ts
@@ -3,14 +3,6 @@ import { IsString, IsNumber } from "class-validator";
 
 export class CreateFileDto {
   @IsString()
-  @ApiProperty({ type: String, description: "파일 이름" })
-  readonly name: string;
-
-  @IsString()
-  @ApiProperty({ type: String, description: "파일 경로" })
-  readonly path: string;
-
-  @IsNumber()
-  @ApiProperty({ type: Number, description: "파일 크기" })
-  readonly size: number;
+  @ApiProperty({ type: File, description: "파일" })
+  readonly file: File;
 }
