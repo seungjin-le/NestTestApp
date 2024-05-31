785ac6f67c6a4291e0eea6628914f6a8f2c7f5a7
Lee Seung Jin <dltmdwls154@gmail.com>
2024-05-31 23:54:50 +0900

    Test

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   // Swagger 설정
   const options = new DocumentBuilder()
     .setTitle("NestJS 연습용 API Docs")
-    .setDescription("NestJS 연습용")
+    .setDescription("NestJS 연습")
     .setVersion("1.0.0")
     .build();
 
