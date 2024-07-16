9a8609217df82b22f742c1aebab5a4c06f0f5774
Lee Seung Jin <dltmdwls154@gmail.com>
2024-07-16 23:43:36 +0900

    Test

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,27 +5,32 @@ import { ApiProperty } from "@nestjs/swagger";
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
-
+  // 이메일
   @ApiProperty({ type: String })
   @Column()
   email: string;
 
+  // 패스워드
   @ApiProperty({ type: String })
   @Column()
   password: string;
 
+  // 닉네임
   @ApiProperty({ type: String })
   @Column()
   nickname: string;
 
+  // 생성일자
   @ApiProperty({ type: String })
   @CreateDateColumn()
   createdAt: Date;
 
+  // 수정일자
   @ApiProperty({ type: String })
   @UpdateDateColumn()
   updatedAt: Date;
 
+  // 삭제일자
   @ApiProperty({ type: Date })
   @DeleteDateColumn()
   deletedAt: Date;
