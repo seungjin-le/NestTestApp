399e55f03aac05954a4cf4cdd3d996500f773122
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-19 23:55:20 +0900

    Test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,42 +1,43 @@
-import { Module } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthPostLoginController, AuthPostRefreshController } from "./auth.controller";
-import { UserModule } from "../user/user.module";
-import { MongooseModule } from "@nestjs/mongoose";
+import {Module} from "@nestjs/common";
+import {AuthService} from "./auth.service";
+import {AuthPostLoginController, AuthPostRefreshController} from "./auth.controller";
+import {UserModule} from "../user/user.module";
+import {MongooseModule} from "@nestjs/mongoose";
 import AuthSchema from "./auth.schema";
 // import { AuthGuard } from "./auth.guard";
-import { APP_GUARD } from "@nestjs/core";
-import { JwtModule } from "@nestjs/jwt";
-import { ConfigService } from "@nestjs/config";
+import {APP_GUARD} from "@nestjs/core";
+import {JwtModule} from "@nestjs/jwt";
+import {ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [
-    UserModule,
-    MongooseModule.forFeature([
-      {
-        name: "Auth",
-        schema: AuthSchema.schema,
-      },
-    ]),
-    JwtModule.registerAsync({
-      // JWT 모듈
-      global: true,
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        secret: config.get('JWT_SECRET'),
+    imports: [
+        UserModule,
+        MongooseModule.forFeature([
+            {
+                name: "Auth",
+                schema: AuthSchema.schema,
+            },
+        ]),
+        JwtModule.registerAsync({
+            // JWT 모듈
+            global: true,
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                secret: config.get('JWT_SECRET'),
 
-        signOptions: { expiresIn: "1h", algorithm: "HS256" },
-      }),
-    }),
-  ],
-  controllers: [AuthPostRefreshController, AuthPostLoginController],
+                signOptions: {expiresIn: "1h", algorithm: "HS256"},
+            }),
+        }),
+    ],
+    controllers: [AuthPostRefreshController, AuthPostLoginController],
 
-  providers: [
-    AuthService,
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: AuthGuard,
-    // },
-  ],
+    providers: [
+        AuthService,
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuard,
+        },
+    ],
 })
-export class AuthModule {}
+export class AuthModule {
+}
