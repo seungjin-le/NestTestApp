54f8db8cabb96f5db3652588ec15969965493c05
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-26 23:36:28 +0900

    Test

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,15 +1,22 @@
-// import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
-// import { Observable } from "rxjs";
-// import { AuthService } from "./auth.service";
-//
-// @Injectable()
-// export class AuthGuard implements CanActivate {
-//   constructor(private authService: AuthService) {}
-//
-//   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-//     console.log("AuthGuard");
-//     const request = context.switchToHttp().getRequest();
-//     const token = request.headers.authorization?.split(" ")[1];
-//     return this.authService.checkedToken(token);
-//   }
-// }
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor( private authService: AuthService ) {
+  }
+
+  canActivate( context: ExecutionContext ): boolean | Promise<boolean> | Observable<boolean> {
+
+    const request = context.switchToHttp().getRequest();
+    const { originalUrl } = request;
+    if ( originalUrl !== "/api/vi/auth/login" || originalUrl !== "/api/vi/auth/refresh" ) {
+      const token = request.headers.authorization?.split( " " )[ 1 ];
+
+      return this.authService.checkedToken( token );
+    }
+
+    return true;
+  }
+}
