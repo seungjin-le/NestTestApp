077f546d0284d319379eda380a14f57135239c1f
Lee Seung Jin <dltmdwls154@gmail.com>
2024-11-28 23:46:07 +0900

    Test

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { JwtModule } from "@nestjs/jwt";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 import { PostUserModule } from "./post_user/post_user.module";
-import { DbModule } from "./db/db.module";
+// import { DbModule } from "./db/db.module";
 import { EventModule } from "./event/event.module";
 import configuration from "@/config/configuration";
 
@@ -20,36 +20,36 @@ import configuration from "@/config/configuration";
       envFilePath: ".env",
     }),
     ConfigModule.forRoot({}), // 환경 변수 모듈
-    // JwtModule.register({
-    //   // JWT 모듈
-    //   global: true,
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: { expiresIn: "30m", algorithm: "HS256" },
-    // }),
-    // // MongoDB 모듈
-    // MongooseModule.forRootAsync({
-    //   useFactory: () => ({
-    //     uri: process.env.MONGODB_URL,
-    //   }),
-    // }),
-    // EventModule, // 이벤트 모듈
-    // MoviesModule, // 영화 모듈
-    // AuthModule, // 인증 모듈
-    // UserModule, // 사용자 모듈
-    // PostUserModule, // 게시글 모듈
+    JwtModule.register({
+      // JWT 모듈
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: "30m", algorithm: "HS256" },
+    }),
+    // MongoDB 모듈
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URL,
+      }),
+    }),
+    EventModule, // 이벤트 모듈
+    MoviesModule, // 영화 모듈
+    AuthModule, // 인증 모듈
+    UserModule, // 사용자 모듈
+    PostUserModule, // 게시글 모듈
     // DbModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {
-  // constructor(@InjectConnection() private readonly mongooseConnection: Connection) {
-  //   // MongoDB 연결 상태 확인
-  //   const mongooseInstance = this.mongooseConnection;
-  //   if (mongooseInstance.readyState === 1) {
-  //     console.log("MongoDB 연결 성공!");
-  //   } else {
-  //     console.error("MongoDB 연결 실패:", mongooseInstance.readyState);
-  //   }
-  // }
+  constructor(@InjectConnection() private readonly mongooseConnection: Connection) {
+    // MongoDB 연결 상태 확인
+    const mongooseInstance = this.mongooseConnection;
+    if (mongooseInstance.readyState === 1) {
+      console.log("MongoDB 연결 성공!");
+    } else {
+      console.error("MongoDB 연결 실패:", mongooseInstance.readyState);
+    }
+  }
 }
