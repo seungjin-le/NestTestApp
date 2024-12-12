2886004b74652bd2b851e911b369d7ad326780ce
Lee Seung Jin <dltmdwls154@gmail.com>
2024-12-12 23:05:35 +0900

    Test

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,7 +1,31 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { SocketService } from './socket.service';
 
 @WebSocketGateway()
 export class SocketGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly socketService: SocketService) {}
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string, @ConnectedSocket() client: Socket): void {
+    console.log('Received message:', message);
+    this.server.emit('message', message);
+  }
+
+  @SubscribeMessage('join')
+  handleJoin(@MessageBody() room: string, @ConnectedSocket() client: Socket): void {
+    client.join(room);
+    console.log(`Client joined room: ${room}`);
+    this.server.to(room).emit('message', `A new client has joined the room: ${room}`);
+  }
+
+  @SubscribeMessage('leave')
+  handleLeave(@MessageBody() room: string, @ConnectedSocket() client: Socket): void {
+    client.leave(room);
+    console.log(`Client left room: ${room}`);
+    this.server.to(room).emit('message', `A client has left the room: ${room}`);
+  }
 }
