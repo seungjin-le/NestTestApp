7ccb5821d5d89bdd86d09f20c17a43a5ba2392a9
Lee Seung Jin <dltmdwls154@gmail.com>
2024-12-19 23:29:33 +0900

    Test

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   // Swagger UI가 사용할 endpoint 설정
   SwaggerModule.setup("api-docs", app, document);
   app.enableCors(); // CORS 설정 추가
-  // 애플리케이션 리스닝
+
   await app.listen(3000, "0.0.0.0");
 }
 
