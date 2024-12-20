6f75c0a818ac564a2723f1717f212004b795a209
Lee Seung Jin <dltmdwls154@gmail.com>
2024-12-20 23:29:03 +0900

    Test

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ async function bootstrap() {
   // 글로벌 파이프 설정
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
+      whitelist: true, // DTO에 정의되지 않은(데코레이터가 없는) 속성들을 자동으로 제거
+      forbidNonWhitelisted: true, // "true로 설정하면, 허용되지 않은 속성들을 제거하는 대신 validator가 에러를 발생
+      transform: true, // DTO에서 타입 변환
     })
   );
 
