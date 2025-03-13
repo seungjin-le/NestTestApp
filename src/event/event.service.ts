db460744f838379f3be3b8828a57ec5e6d3f6a60
Lee Seung Jin <dltmdwls154@gmail.com>
2025-03-13 22:39:24 +0900

    Test

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -5,6 +5,7 @@ import { interval, Observable, Subject } from "rxjs";
 export class EventService {
   private testEvent: Record<string, Subject<MessageEvent>> = {};
 
+  //  Constructor
   constructor() {
     // 모든 사용자에게 주기적으로 메시지를 보내는 interval 설정
     interval(3000).subscribe(() => {
@@ -15,11 +16,13 @@ export class EventService {
     });
   }
 
+  //  사용자에게 메시지를 받는 함수
   handleConnetcion(id: string): Observable<MessageEvent> {
     if (!this.testEvent[id]) this.testEvent[id] = new Subject<MessageEvent>();
     return this.testEvent[id].asObservable();
   }
 
+  //  사용자에게 메시지를 보내는 함수
   sendTestEvent(id: string, event: MessageEvent) {
     console.log("sendTestEvent", id, event, this.testEvent);
     if (this.testEvent[id]) this.testEvent[id].next(event);
